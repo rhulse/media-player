d703ec106b52e5a8f63671f9d89d8a9a2fac4db1
Richard Hulse <richard.hulse@radionz.co.nz>
2008-11-24 12:10:43 +1300

    refactored cookie playlist plugin to return a cookie object
    Sometimes two playlists are needed, so we need an object for each
    to ensure that that the two don't get mixed up!
    
    In this case one is a queue for passing items between windows,
    and the other is the real playlist i.e. the items that are
    showing in the user playlist.

--- jquery.cookieplaylist.js
+++ jquery.cookieplaylist.js
@@ -3,6 +3,8 @@
 
 	This plugin manages a playlist of integers stored in a cookie
 
+	The initialise function return a cookie class (so we can have more thean)
+
 	Copyright (c) 2008 Radio New Zealand
 	Author: Richard Hulse
 	This software is released under the GNU GPL version 2 or later. <http://www.opensource.org/licenses/gpl-2.0.php>
@@ -14,78 +16,85 @@
 
 (function($) {
 
-  // Private Variables
-	var DEBUG = 1;
-
-	var playlist = new Array();
-
-	var cookie_name = 'audio_playlist';
-
-	var paused_at = 0;
-
   // Public Variables and Methods
   $.cookiePlaylist = {
 
-		initialise: function( playlist_name ) {
-			cookie_name	= playlist_name || cookie_name;
-			this.load();
-		},
+		initialise: function( name, max_age ) {
+			name		= name || 'audio_playlist';
+			max_age	= max_age || 14;
+			return new cookie( name, max_age );
+		}
+	}
 
-		add: function(id){
+	function cookie( name, max_age ) {
+
+		this.cookie_name 	= name;
+		this.max_age			= max_age;
+		this.playlist 		= new Array();
+
+		this.add = function(id){
+			this.load();
 			if( ! in_array( id, playlist ) ) {
-				playlist.push(id);
+				this.playlist.push(id);
 				this.save();
 			}
-		},
+		};
 
-		remove: function(id){
+		this.remove = function(id){
+			this.load();
 			var i = 0;
-			while ( i < playlist.length ) {
-				if ( playlist[i] == id ) {
-					playlist.splice( i, 1);
+			while ( i < this.playlist.length ) {
+				if ( this.playlist[i] == id ) {
+					this.playlist.splice( i, 1);
 				}
 				else {
 					i++;
 				}
 			}
 			this.save();
-		},
+		};
 
-		pop: function(){
-			return playlist.pop;
-		},
+		this.pop = function(){
+			this.load();
+			return this.playlist.pop;
+		};
 
-		save: function(){
-			$.cookie( cookie_name, playlist.join(), {duration: 14} );
+		this.save = function(){
+			$.cookie( this.cookie_name, this.playlist.join(), {duration: this.max_age} );
 			// console.log("playlist: saved playlist - " + playlist);
-		},
+		};
 
-		clear: function(id){
-			$.cookie( cookie_name, null );
-		},
+		this.clear = function(id){
+			$.cookie( this.cookie_name, null );
+		};
 
-		load: function(){
+		this.load = function(){
 			var temp_playlist = new Array();
-			var pstr = $.cookie( cookie_name );
+			var pstr = $.cookie( this.cookie_name );
 			if( pstr ){
 				temp_playlist = pstr.split(',');
 			}
-			playlist = temp_playlist;
+			this.playlist = temp_playlist;
 			// console.log("playlist: loaded - " + playlist.join() );
+		};
+
+		this.show = function(){
+			this.load();
+			console.log( "Playlist '" + this.cookie_name +  "': " + this.playlist.join() );
+		}
+
+		function in_array(needle, haystack, strict) {
+			// original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	    var found = false, key, strict = !!strict;
+	    for (key in haystack) {
+	      if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {
+					found = true;
+		      break;
+		    }
+		  }
+		 return found;
 		}
 
 	};
-
-	function in_array(needle, haystack, strict) {
- 		// original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    var found = false, key, strict = !!strict;
-    for (key in haystack) {
-      if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {
-       	found = true;
-	      break;
-	    }
-	  }
-	 return found;
-	}
 
 })(jQuery);
